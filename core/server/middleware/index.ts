c35299bac942f0f266c8b5291266eacfc6f844a7
FluxStack Team <team@fluxstack.dev>
2025-10-04 13:52:42 -0300

    helpers

--- core/server/middleware/index.ts
+++ core/server/middleware/index.ts
@@ -3,14 +3,30 @@
  * FluxStack middleware infrastructure exports
  */
 
-export { 
-  errorHandlingMiddleware, 
-  notFoundMiddleware, 
-  createError, 
-  asyncHandler 
+export {
+  errorHandlingMiddleware,
+  notFoundMiddleware,
+  createError,
+  asyncHandler
 } from './errorHandling.js'
 
-export type { 
-  ErrorHandlingOptions, 
-  FluxStackError 
+export type {
+  ErrorHandlingOptions,
+  FluxStackError
 } from './errorHandling.js'
+
+// Elysia Middleware Helpers
+export {
+  createMiddleware,
+  createDerive,
+  createGuard,
+  createRateLimit,
+  composeMiddleware,
+  isDevelopment,
+  isProduction,
+  isTest
+} from './elysia-helpers.js'
+
+export type {
+  MiddlewareOptions
+} from './elysia-helpers.js'
